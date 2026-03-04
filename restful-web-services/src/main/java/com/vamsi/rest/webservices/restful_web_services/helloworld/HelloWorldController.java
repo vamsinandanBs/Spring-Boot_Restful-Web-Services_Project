043c2632339267679e6b4a09441474d405e02ce7
Vamsinandan BS <61873858+vamsinandanBs@users.noreply.github.com>
2026-03-04 16:10:13 +0530

    GetMapping
    GetMapping, RequestMapping

---
+++ restful-web-services/src/main/java/com/vamsi/rest/webservices/restful_web_services/helloworld/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.vamsi.rest.webservices.restful_web_services.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    //@RequestMapping(method = RequestMethod.GET, path  = "/Rest_Service")
+    @GetMapping(path = "/Rest_Service")
+    public String helloWorld(){
+        return "Hi Checking Web Service";
+    }
+}
