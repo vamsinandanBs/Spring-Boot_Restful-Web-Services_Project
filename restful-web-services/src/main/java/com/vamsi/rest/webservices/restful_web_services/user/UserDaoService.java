191b226c785ca592a77b99a6ebb133a4e3306cd1
Vamsinandan BS <61873858+vamsinandanBs@users.noreply.github.com>
2026-03-06 00:56:12 +0530

    get, post, status code
    get, post, status code

--- restful-web-services/src/main/java/com/vamsi/rest/webservices/restful_web_services/user/UserDaoService.java
+++ restful-web-services/src/main/java/com/vamsi/rest/webservices/restful_web_services/user/UserDaoService.java
@@ -5,26 +5,38 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class UserDaoService {
 
     private static List<User> users =new  ArrayList<>();
 
+    private static int usersCount=0;
+
     static {
-        users.add(new User(1,"Vamsi", LocalDate.now().minusYears(26)));
-        users.add(new User(2,"Kiran", LocalDate.now().minusYears(46)));
-        users.add(new User(3,"Suresh", LocalDate.now().minusYears(55)));
-        users.add(new User(4,"Lakshmi", LocalDate.now().minusYears(45)));
+        users.add(new User(++usersCount,"Vamsi", LocalDate.now().minusYears(26)));
+        users.add(new User(++usersCount,"Kiran", LocalDate.now().minusYears(46)));
+        users.add(new User(++usersCount,"Suresh", LocalDate.now().minusYears(55)));
+        users.add(new User(++usersCount,"Lakshmi", LocalDate.now().minusYears(45)));
     }
 
     public  List<User> findAll() {
         return users;
     }
 
-//    public User findOne(int id) {
-//        Predicate<? super User> predicate = user -> user.getId().equals(id);
-//        return users.stream().filter(predicate).findFirst().get();
-//    }
+    public  User  insertUser(User user) {
+        user.setId((++usersCount));
+        users.add(user);
+
+        return user;
+    }
+
+    public User findOne(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+       //return users.stream().filter(predicate).findFirst().get();
+        // if not found it gives exception instead adding or else with default value
+        return users.stream().filter(predicate).findFirst().orElse(null);
+    }
 
 }
--- restful-web-services/src/main/java/com/vamsi/rest/webservices/restful_web_services/user/UserResource.java
+++ restful-web-services/src/main/java/com/vamsi/rest/webservices/restful_web_services/user/UserResource.java
@@ -1,5 +1,45 @@
 package com.vamsi.rest.webservices.restful_web_services.user;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
 public class UserResource {
-    
+
+    private UserDaoService service;
+
+    public UserResource(UserDaoService service){
+        this.service=service;
+    }
+
+    @GetMapping("/users")
+    public List<User> RetriveAllUsers(){
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User RetriveUser(@PathVariable int id){
+         //service.findOne(id);
+        User user = service.findOne(id);
+
+        if(user==null)
+            throw new UserNotFoundException("id:"+id);
+
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@RequestBody User user){
+        User savedUser= service.insertUser(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
+    }
 }
