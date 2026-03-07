18580774e2f4606540f91d7158d7b1f95eebb0ad
Vamsinandan BS <61873858+vamsinandanBs@users.noreply.github.com>
2026-03-07 10:33:21 +0530

    Create ErrorDetails.java

---
+++ restful-web-services/src/main/java/com/vamsi/rest/webservices/restful_web_services/exception/ErrorDetails.java
@@ -0,0 +1,30 @@
+package com.vamsi.rest.webservices.restful_web_services.exception;
+
+import java.time.LocalDate;
+
+public class ErrorDetails {
+
+    private LocalDate timestamp;
+    private String message;
+    private String details;
+
+    public ErrorDetails(LocalDate timestamp, String message, String details) {
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    public LocalDate getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    
+}
